b2f3540ca892f381d048fdd8a98b9c2a4543403c
Matt Jones <matt@Matts-MacBook-Pro.local>
2013-02-20 23:35:31 -0500

    added stuff

--- py_scripts/comic_pro.py
+++ py_scripts/comic_pro.py
@@ -24,11 +24,6 @@
 
 # THIS IS A PRODUCTION SCRIPT DO NOT MODIFY!!
 
-#need to move function to other lib file
-#clean code of testing items
-#figure out string processing to get the series and set the series id
-
-
 import sys
 import csv
 import getopt
@@ -42,23 +37,13 @@ if "/home/matt/ComicTracker-PG/comic_inserts" not in sys.path:
 
 import ct_functions as ct_funct
 
-#---------------------------------------#
-# LIST_OF_TITLES			#
-#					#
-# a dictionary list of comics to be 	#
-# added 				#
-#					#
-#					#
-#---------------------------------------#
-
-# COMIC_FILE = '../comic_inserts/batman_1940'
-#COMIC_ISSUE = ''
 COMIC_FILE = ''
 SERIES = ''
+SERIES_ID = ''
 
 db_con, db_cur = ct_funct.db_connection() #open a connection
 
-COMIC_FILE = ct_funct.get_comic_file()
+COMIC_FILE = ct_funct.get_comic_file() #get the file to be read
 
 reader = csv.DictReader(open(COMIC_FILE, 'rw'), fieldnames = ['series', 'issue', 'date', 'isbn', 'quanity'], dialect='excel-tab') #this reads a tab seperated file
 
@@ -67,19 +52,16 @@ for row in reader:
 	comicinfo.append(row) #read all dict's into list one issue per line
 
 
-one = 'Birds Of Prey(2011)'
-matt = ct_funct.get_series_id(one, db_cur)
-print matt
+SERIES_ID = ct_funct.get_series_id(SERIES, db_cur) #get the series id
 
-s = {'series_id' : matt } # get the correct series id 
+s = {'series_id' : SERIES_ID } # create a dict entry for the series id 
 
 for item in comicinfo:
 	item.update (s) #append the series id to the list
 
 for item in comicinfo:
-#	print 'hello'
 	print item # print the list as a check
-#	db_cur.execute("INSERT INTO ComicIssue(fkey_defaultseries_id, comicissue_num, isbn, pubdate, quanity) VALUES ('%s','%s','%s','%s','%s')" % (item["series_id"], item["issue"], item["isbn"], item["date"], item["quanity"]))
+	db_cur.execute("INSERT INTO ComicIssue(fkey_defaultseries_id, comicissue_num, isbn, pubdate, quanity) VALUES ('%s','%s','%s','%s','%s')" % (item["series_id"], item["issue"], item["isbn"], item["date"], item["quanity"]))
 
 db_con.commit() #commit all changes
 
